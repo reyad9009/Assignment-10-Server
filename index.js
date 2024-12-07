f87f4ec255064756064c9d6f3a14af0a98a895ec
reyad9009 <tarekgithub09@gmail.com>
2024-12-08 04:08:17 +0600

    Get equipment data by logged-in user's email

--- index.js
+++ index.js
@@ -27,15 +27,39 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const coffeeCollection = client.db('equipmentsDB').collection('equipments');
+        const EquipmentCollection = client.db('equipmentsDB').collection('equipments');
 
-        //post data in to mongodb
+        //post data in mongodb
         app.post('/equipments', async (req, res) => {
             const newAddedEquipment = req.body;
             console.log(newAddedEquipment)
-            const result = await coffeeCollection.insertOne(newAddedEquipment);
+            const result = await EquipmentCollection.insertOne(newAddedEquipment);
             res.send(result);
         })
+
+        //get data from mongodb
+        app.get('/equipments', async (req, res) => {
+            const cursor = EquipmentCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // Get equipment data by logged-in user's email
+        app.get('/equipments', async (req, res) => {
+            const { userEmail } = req.query;
+            if (!userEmail) {
+                return res.status(400).send({ message: "User email is required" }); 
+            }
+            const query = { userEmail: userEmail }; 
+            const result = await EquipmentCollection.filter(query).toArray(); 
+            res.send(result); 
+        });
+
+
+
+
+
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -55,3 +79,33 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Sports Equipments server is running on port: ${port}`);
 })
+
+
+
+
+
+// _id: ObjectId('67538476e7c808f5ca22bb29')
+// image: "https://example.com/images/table-tennis-paddle.jpg",
+// itemName: "Table Tennis Paddle",
+// categoryName: "Table Tennis",
+// description: "Ergonomic paddle with high-speed rubber.",
+// price: "1000",
+// rating: "4.4",
+// customization: "Handle wrap",
+// processingTime: "2-3 days",
+// stockStatus: "40"
+// userEmail: "tarekhossen105@gmail.com"
+// userName: "Tarek Rahman"
+
+// _id: ObjectId('67538476e7c808f5ca26562bb29')
+// image: "https://example.com/images/table-tennis-paddle2.jpg",
+// itemName: "Table Tennis Paddle",
+// categoryName: "Table Tennis",
+// description: "Ergonomic paddle with high-speed rubber.",
+// price: "1000",
+// rating: "4.4",
+// customization: "Handle wrap",
+// processingTime: "2-3 days",
+// stockStatus: "80"
+// userEmail: "reyad900@gmail.com"
+// userName: "Reyad"
